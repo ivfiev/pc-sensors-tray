32c365733a72c745531f5e215d02a7834c363b5c
ivfiev <filjko@ya.ru>
2023-09-25 18:59:11 +0100

    Refresh option

--- tray.go
+++ tray.go
@@ -19,7 +19,7 @@ func onReady() {
 	args := os.Args[1:]
 	reader := getSensorReader(args)
 	svc := icon.NewIconService()
-	mQuit := systray.AddMenuItem("Quit", "Quit")
+	mRefresh := systray.AddMenuItem("Refresh", "Refresh")
 	mEntries := make([]*systray.MenuItem, 0)
 	ticker := time.NewTicker(3 * time.Second)
 	var lastResult types.Result
@@ -34,35 +34,39 @@ func onReady() {
 			needsUpdate := changeSignificant || noUpdateForAWhile
 
 			if needsUpdate {
-				bytes, err := svc.GetIcon(newResult)
-				if err != nil {
-					log.Fatal(err)
-				}
-				systray.SetIcon(bytes)
-
-				lines := newResult.Lines()
-				if len(mEntries) == 0 && len(lines) > 0 {
-					for _, line := range lines {
-						mEntries = append(mEntries, systray.AddMenuItem(line, line))
-					}
-				} else if len(lines) > 0 {
-					for i, entry := range mEntries {
-						entry.SetTitle(lines[i])
-						entry.SetTooltip(lines[i])
-					}
-				}
-
+				update(svc, newResult, &mEntries)
 				lastResult = newResult
 				lastUpdate = time.Now()
 			}
-		case <-mQuit.ClickedCh:
-			systray.Quit()
+		case <-mRefresh.ClickedCh:
+			newResult := reader()
+			update(svc, newResult, &mEntries)
 		}
 	}
 }
 
 func onExit() {
 
+}
+
+func update(svc icon.IconService, result types.Result, mEntries *[]*systray.MenuItem) {
+	bytes, err := svc.GetIcon(result)
+	if err != nil {
+		log.Fatal(err)
+	}
+	systray.SetIcon(bytes)
+
+	lines := result.Lines()
+	if len(*mEntries) == 0 && len(lines) > 0 {
+		for _, line := range lines {
+			*mEntries = append(*mEntries, systray.AddMenuItem(line, line))
+		}
+	} else if len(lines) > 0 {
+		for i, entry := range *mEntries {
+			entry.SetTitle(lines[i])
+			entry.SetTooltip(lines[i])
+		}
+	}
 }
 
 func getSensorReader(args []string) func() types.Result {
